af17fb461f02727983af82e8c2791b7b59472c9f
Amadur92 <69795882+Amadur92@users.noreply.github.com>
2022-06-07 17:19:19 +0300

    static added

--- Web_ChGK/urls.py
+++ Web_ChGK/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.conf import settings
 
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path('admin/', admin.site.urls),
     path('', include('question_packages.urls')),
 ]
