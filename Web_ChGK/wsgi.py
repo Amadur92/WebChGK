421bbce1034413fad3a09251ebbc141b975bb63b
Amadur92 <69795882+Amadur92@users.noreply.github.com>
2022-06-08 12:55:51 +0300

    removed pipfile

--- Web_ChGK/wsgi.py
+++ Web_ChGK/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('PycharmProjects\TryWebOnDjango\Web_ChGK')
+sys.path.append('PycharmProjects\TryWebOnDjango\Web_ChGK\Web_ChGK')
 
 from django.core.wsgi import get_wsgi_application
 
