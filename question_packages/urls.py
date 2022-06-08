36182d3ca00610757a67fcbf6216e07e90723280
Amadur92 <69795882+Amadur92@users.noreply.github.com>
2022-06-08 11:50:22 +0300

    urls changed

--- question_packages/urls.py
+++ question_packages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-
+#path('', views.show_packages, name='home'),
 urlpatterns = [
-    path('', views.show_packages, name='home'),
+   
     path('sq/', views.SingleQuestionPageView.as_view(), name = 'single_question'),
     path('about/', views.AboutPageView.as_view(), name = 'about')
 ]
