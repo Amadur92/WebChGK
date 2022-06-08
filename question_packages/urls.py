9906d4251b9f68d89486bda668c65dbf64d2d855
Amadur92 <69795882+Amadur92@users.noreply.github.com>
2022-06-08 13:02:47 +0300

    returned old urls

--- question_packages/urls.py
+++ question_packages/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.show_packages, name='home'),]
-#    path('sq/', views.SingleQuestionPageView.as_view(), name = 'single_question'),
-#    path('about/', views.AboutPageView.as_view(), name = 'about')
-#]
-#
+    path('', views.show_packages, name='home'),
+    path('sq/', views.SingleQuestionPageView.as_view(), name = 'single_question'),
+    path('about/', views.AboutPageView.as_view(), name = 'about')
+]
