603363306add4bb894159b6673ddbeeb98d270f9
Amadur92 <69795882+Amadur92@users.noreply.github.com>
2022-06-08 12:25:58 +0300

    temporarly commented urls

--- question_packages/urls.py
+++ question_packages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-#path('', views.show_packages, name='home'),
+
 urlpatterns = [
-   
-    path('sq/', views.SingleQuestionPageView.as_view(), name = 'single_question'),
-    path('about/', views.AboutPageView.as_view(), name = 'about')
-]
+    path('', views.show_packages, name='home'),]
+#    path('sq/', views.SingleQuestionPageView.as_view(), name = 'single_question'),
+#    path('about/', views.AboutPageView.as_view(), name = 'about')
+#]
+#
