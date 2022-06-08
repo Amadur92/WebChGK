7a0571d5e725911a726a62536db05a61449cead6
Amadur92 <69795882+Amadur92@users.noreply.github.com>
2022-06-08 12:41:13 +0300

    still trying run heroku

--- Web_ChGK/wsgi.py
+++ Web_ChGK/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_ChGK.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_ChGK.Web_ChGK.settings')
 
 application = get_wsgi_application()
