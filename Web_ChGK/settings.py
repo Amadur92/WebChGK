6a23d3b0ae7e7abce500786e899026b756b87eee
Amadur92 <69795882+Amadur92@users.noreply.github.com>
2022-06-23 16:29:37 +0300

    add CustomUser

--- Web_ChGK/settings.py
+++ Web_ChGK/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'question_packages',
     'favicon',
-    'accounts'
+    'accounts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -88,7 +89,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'users.CustomUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from users.forms import CustomUserCreationForm
 # Create your views here.
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
